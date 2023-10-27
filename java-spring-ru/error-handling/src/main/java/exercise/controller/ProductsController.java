03d1d9d6fe661cb2d93556070c843a43e38038ff
@hexlet/cli <support@hexlet.io>
2023-10-27 05:23:38 +0200

    submit java-spring-ru/error-handling

--- java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
@@ -6,36 +6,36 @@ import exercise.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/products")
+@RequestMapping("/products")
 public class ProductsController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/products")
+    @GetMapping(path = "")
     public List<Product> index() {
         return productRepository.findAll();
     }
 
-    @PostMapping("/products")
+    @PostMapping(path = "")
     @ResponseStatus(HttpStatus.CREATED)
     public Product create(@RequestBody Product product) {
         return productRepository.save(product);
     }
 
     // BEGIN
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     Product show(@PathVariable Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Product update(@PathVariable Long id, @RequestBody Product data) {
         Product product = productRepository.findById(id)
@@ -47,7 +47,7 @@ public class ProductsController {
 
     // END
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping(path = "/{id}")
     public void delete(@PathVariable long id) {
         productRepository.deleteById(id);
     }
